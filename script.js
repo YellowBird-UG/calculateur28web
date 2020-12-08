9ad8bc4cf6a01fba4268f912bde9bc389aecb797
treva-123mutebi <mdavid.trevor@gmail.com>
2020-12-08 14:54:27 +0300

    new calculations

--- script.js
+++ script.js
@@ -3,85 +3,125 @@
 new Vue({
     el: '#app',
     components: {
-      vuejsDatepicker },
-  
+        vuejsDatepicker
+    },
+
     data() {
-      return {
-        calcReturned: false,
-        date: new Date(),
-        cycleSelected: 28,
-        fertileFrom: "",
-        fertileUntil: "",
-        dueDate: "",
-        fullDate: "" };
-  
+        return {
+            calcReturned: false,
+            date: new Date(),
+            cycleSelected: 28,
+            fertileFrom: "",
+            fertileUntil: "",
+            menstruationbegins: "",
+            menstruationends: "",
+            ovulationday: "",
+            dueDate: "",
+            fullDate: ""
+        };
+
     },
     methods: {
-      startCalc: function () {
-        this.calcReturned = true;
-      },
-      calcDate: function () {
-        var day = parseInt(document.querySelector("span.day.selected").innerHTML);
-        var month = document.querySelector("span.month.selected").innerHTML;
-        var year = parseInt(document.querySelector("span.year.selected").innerHTML);
-  
-        var months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"];
-  
-  
-        months.forEach(function (value, i) {
-          if (value === month && i <= 8) {
-            i++;
-            month = "0" + i.toString();
-          } else if (value === month && i >= 9) {
-            i++;
-            month = i.toString();
-          }
-        });
-  
-        this.fullDate = year + "" + month + "" + day;
-      },
-      calculateFertileBegin: function () {
-        this.calcDate();
-        if (this.cycleSelected === 28) {
-          this.fertileFrom = moment(this.fullDate, 'YYYYMMDD').add(12, 'days').format('MMMM Do YYYY');
-        } else {
-          this.fertileFrom = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 12, 'days').format('MMMM Do YYYY');
+        startCalc: function() {
+            this.calcReturned = true;
+        },
+        calcDate: function() {
+            var day = parseInt(document.querySelector("span.day.selected").innerHTML);
+            var month = document.querySelector("span.month.selected").innerHTML;
+            var year = parseInt(document.querySelector("span.year.selected").innerHTML);
+
+            var months = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            ];
+
+
+            months.forEach(function(value, i) {
+                if (value === month && i <= 8) {
+                    i++;
+                    month = "0" + i.toString();
+                } else if (value === month && i >= 9) {
+                    i++;
+                    month = i.toString();
+                }
+            });
+
+            this.fullDate = year + "" + month + "" + day;
+        },
+
+        calculateFertileBegin: function() {
+            this.calcDate();
+            if (this.cycleSelected === 28) {
+                this.fertileFrom = moment(this.fullDate, 'YYYYMMDD').add(12, 'days').format('MMMM Do YYYY');
+            } else {
+                this.fertileFrom = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 12, 'days').format('MMMM Do YYYY');
+            }
+        },
+        calculateFertileEnds: function() {
+            //this.calcDate(); is called from calculateFertileBegin
+            if (this.cycleSelected === 28) {
+                this.fertileUntil = moment(this.fullDate, 'YYYYMMDD').add(16, 'days').format('MMMM Do YYYY');
+            } else {
+                this.fertileUntil = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 16, 'days').format('MMMM Do YYYY');
+            }
+        },
+        calculateMbegins: function() {
+            //this.calcDate(); is called from calculateFertileBegin
+            if (this.cycleSelected === 28) {
+                this.menstruationbegins = moment(this.fullDate, 'YYYYMMDD').add(28, 'days').format('MMMM Do YYYY');
+            } else {
+                this.menstruationbegins = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected, 'days').format('MMMM Do YYYY');
+            }
+        },
+        calculateMends: function() {
+            //this.calcDate(); is called from calculateFertileBegin
+            if (this.cycleSelected === 28) {
+                this.menstruationends = moment(this.fullDate, 'YYYYMMDD').add(33, 'days').format('MMMM Do YYYY');
+            } else {
+                this.menstruationends = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected + 5, 'days').format('MMMM Do YYYY');
+            }
+        },
+        calculateOday: function() {
+            //this.calcDate(); is called from calculateFertileBegin
+            if (this.cycleSelected === 28) {
+                this.ovulationday = moment(this.fullDate, 'YYYYMMDD').add(15, 'days').format('MMMM Do YYYY');
+            } else {
+                this.ovulationday = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 15, 'days').format('MMMM Do YYYY');
+            }
+        },
+
+
+
+
+        calculateDueDate: function() {
+            //this.calcDate(); is called from calculateFertileBegin
+            if (this.cycleSelected === 28) {
+                this.dueDate = moment(this.fullDate, 'YYYYMMDD').add(280, 'days').format('MMMM Do YYYY');
+            } else {
+                this.dueDate = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 280, 'days').format('MMMM Do YYYY');
+            }
+        },
+        resetCalc: function() {
+            this.calcReturned = false;
+            this.date = new Date();
+            this.cycleSelected = 28;
+            this.fertileFrom = "";
+            this.fertileUntil = "";
+            this.dueDate = "";
+            this.fullDate = "";
+            this.menstruationbegins = "";
+            this.menstruationends = "";
+            this.ovulationday = "";
         }
-      },
-      calculateFertileEnds: function () {
-        //this.calcDate(); is called from calculateFertileBegin
-        if (this.cycleSelected === 28) {
-          this.fertileUntil = moment(this.fullDate, 'YYYYMMDD').add(16, 'days').format('MMMM Do YYYY');
-        } else {
-          this.fertileUntil = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 16, 'days').format('MMMM Do YYYY');
-        }
-      },
-      calculateDueDate: function () {
-        //this.calcDate(); is called from calculateFertileBegin
-        if (this.cycleSelected === 28) {
-          this.dueDate = moment(this.fullDate, 'YYYYMMDD').add(280, 'days').format('MMMM Do YYYY');
-        } else {
-          this.dueDate = moment(this.fullDate, 'YYYYMMDD').add(this.cycleSelected - 28 + 280, 'days').format('MMMM Do YYYY');
-        }
-      },
-      resetCalc: function () {
-        this.calcReturned = false;
-        this.date = new Date();
-        this.cycleSelected = 28;
-        this.fertileFrom = "";
-        this.fertileUntil = "";
-        this.dueDate = "";
-        this.fullDate = "";
-      } } });
+    }
+});
